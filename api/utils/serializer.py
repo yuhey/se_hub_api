3f3674993a846e5e2607039a0f0546fd35e7a5fb
yuhey <yuhei0825@hotmail.com>
2020-12-02 17:09:35 +0900

    add bp serializer

--- api/utils/serializer.py
+++ api/utils/serializer.py
@@ -1,2 +1,1 @@
-FOLLOW_SERIALIZER = 'id, followed__name, followed__description, followed__img'
-FOLLOWED_SERIALIZER = 'id, follow__name, follow__description, follow__img'
+BP_SERIALIZER = 'id, name, description, img'
--- api/views/bp/list.py
+++ api/views/bp/list.py
@@ -1,10 +1,11 @@
+from django.db.models import F
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from api.models.bp import Bp
 from api.utils import utils
-from api.utils.serializer import FOLLOW_SERIALIZER, FOLLOWED_SERIALIZER
+from api.utils.serializer import BP_SERIALIZER
 
 
 class BpListAPI(APIView):
@@ -14,17 +15,20 @@ class BpListAPI(APIView):
 
         bp_list = utils.get_bp_list(group_id)
         bp_qs = Bp.objects.filter(follow__id__in=bp_list)
+        bp_qs.annotate(followed__name=F('name'), followed__description=F('description'), followed__img=F('img'))
 
         followed_qs = Bp.objects.filter(followed__id=group_id)
         followed_qs = followed_qs.exclude(follow__id__in=bp_list)
+        followed_qs.annotate(follow__name=F('name'), follow__description=F('description'), follow__img=F('img'))
 
         follow_qs = Bp.objects.filter(follow__id=group_id)
         follow_qs = follow_qs.exclude(followed__id__in=bp_list)
+        follow_qs.annotate(followed__name=F('name'), followed__description=F('description'), followed__img=F('img'))
 
         bp_dict = {
-            'bp': bp_qs.values(FOLLOW_SERIALIZER),
-            'follow': follow_qs.values(FOLLOW_SERIALIZER),
-            'followed': followed_qs.values(FOLLOWED_SERIALIZER),
+            'bp': bp_qs.values(BP_SERIALIZER),
+            'follow': follow_qs.values(BP_SERIALIZER),
+            'followed': followed_qs.values(BP_SERIALIZER),
         }
 
         return Response(bp_dict, status=status.HTTP_200_OK)
