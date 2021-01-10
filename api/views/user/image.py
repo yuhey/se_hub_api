2f9c5116cc63717755c3538ccf8ac20faa78cda0
yuhey <yuhei0825@hotmail.com>
2021-01-10 18:19:21 +0900

    イメージ変更API(ユーザー&グループ)

--- api/views/user/image.py
+++ api/views/user/image.py
@@ -1,8 +1,11 @@
+import os
+
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from api.models.user import User
+from se_hub_api import settings
 
 
 class UserImageAPI(APIView):
@@ -11,7 +14,18 @@ class UserImageAPI(APIView):
     def put(request, user_id):
 
         # ユーザー画像取得
-        img = request.files.get('user_img')
+        img = request.FILES.get('user_img')
+
+        if not img:
+            return Response([], status=status.HTTP_204_NO_CONTENT)
+
+        # ユーザー画像のパスを作成
+        USER_IMAGE_PATH = os.path.join(settings.BASE_DIR, 'media', 'img', img.name)
+        print(USER_IMAGE_PATH)
+
+        # ユーザー画像を削除する
+        if os.path.isfile(USER_IMAGE_PATH):
+            os.remove(USER_IMAGE_PATH)
 
         # ユーザー情報を更新する
         user = User.objects.filter(id=user_id).first()
