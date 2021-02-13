99d99b810d3ceab807190bd8e97d2247532e0b88
yuhey <yuhei0825@hotmail.com>
2021-02-13 18:13:43 +0900

    MessageAPI

--- api/views/message/list.py
+++ api/views/message/list.py
@@ -22,5 +22,7 @@ class MessageListAPI(APIView):
             (Q(from_user__id=user_id) | Q(to_user__id=user_id)) & Q(message__isnull=True)).order_by('insert_datetime')
         message_qs = utils.get_qs_for_count(message_qs, count, MESSAGE_TITLE_COUNT)
 
-        return Response(message_qs.values('id', 'title', 'description', 'from_user__img', 'from_user__name',
-                                          'from_user__group__name'), status=status.HTTP_200_OK)
+        return Response(message_qs.values('id', 'title', 'description', 'from_user__id', 'from_user__img',
+                                          'from_user__name', 'from_user__group__name', 'to_user__id', 'to_user__img',
+                                          'to_user__name', 'to_user__group__name'),
+                        status=status.HTTP_200_OK)
