85fc78790e76ef4e8b7fc7aacc5574e90295f193
yuhey <yuhei0825@hotmail.com>
2021-03-02 00:04:03 +0900

    BPApi(検索、ボタン処理周り)

--- api/views/bp/list.py
+++ api/views/bp/list.py
@@ -28,17 +28,20 @@ class BpListAPI(APIView):
         if bp_status == BP:
             bp_qs = Bp.objects.filter(followed__id__in=bp_list)
             bp_vqs = bp_qs.values(user__id=F('followed__id'), user__name=F('followed__name'),
-                                  user__description=F('followed__description'), user__img=F('followed__img'))
+                                  user__description=F('followed__description'), user__img=F('followed__img'),
+                                  user__group__name=F('followed__group__name'))
         elif bp_status == RQ:
             bp_qs = Bp.objects.filter(followed__id=user_id)
             bp_qs = bp_qs.exclude(follow__id__in=bp_list)
             bp_vqs = bp_qs.values(user__id=F('follow__id'), user__name=F('follow__name'),
-                                  user__description=F('follow__description'), user__img=F('follow__img'))
+                                  user__description=F('follow__description'), user__img=F('follow__img'),
+                                  user__group__name=F('follow__group__name'))
         elif bp_status == WT:
             bp_qs = Bp.objects.filter(follow__id=user_id)
             bp_qs = bp_qs.exclude(followed__id__in=bp_list)
             bp_vqs = bp_qs.values(user__id=F('followed__id'), user__name=F('followed__name'),
-                                  user__description=F('followed__description'), user__img=F('followed__img'))
+                                  user__description=F('followed__description'), user__img=F('followed__img'),
+                                  user__group__name=F('followed__group__name'))
         elif bp_status == NN:
             user_qs = User.objects.filter(can_find_name=True)
             if search_string:
@@ -47,7 +50,7 @@ class BpListAPI(APIView):
             user_qs = user_qs.exclude(id=user_id)
             if bp_relative_list:
                 user_qs = user_qs.exclude(id__in=bp_relative_list)
-            bp_vqs = user_qs.values(user__id=F('id'), user__name=F('name'),
-                                    user__description=F('description'), user__img=F('img'))
+            bp_vqs = user_qs.values(user__id=F('id'), user__name=F('name'), user__description=F('description'),
+                                    user__img=F('img'), user__group__name=F('group__name'))
 
         return Response(bp_vqs, status=status.HTTP_200_OK)
